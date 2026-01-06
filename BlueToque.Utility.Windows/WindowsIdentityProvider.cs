c9a17cbcff38860c8abc8b84adff4f2dfde84c15
Michael Coyle <Michael.Coyle@BlueToque.ca>
2026-01-05 20:08:03 -0800

    Added system identity infrastructure

---
+++ BlueToque.Utility.Windows/WindowsIdentityProvider.cs
@@ -0,0 +1,33 @@
+﻿namespace BlueToque.Utility.Windows
+{
+    public static class IdentityHelper
+    {
+        /// <summary>
+        /// Call this from windows to initialze the windows identity provider
+        /// </summary>
+        /// <param name="systemIdentity"></param>
+        public static void InitializeWindows(this SystemIdentity systemIdentity) => systemIdentity.Register(new WindowsIdentityProvider());
+    }
+
+    /// <summary>
+    /// Implement identity on windows
+    /// </summary>
+    class WindowsIdentity : IIdentity
+    {
+        public string Name { get; set; } = "";
+
+        public string Issuer => "Windows";
+    }
+
+    /// <summary>
+    /// The windows identity providers
+    /// </summary>
+    public class WindowsIdentityProvider : IIdentityProvider
+    {
+        public IIdentity GetCurrent() => 
+            new WindowsIdentity()
+            {
+                Name = System.Security.Principal.WindowsIdentity.GetCurrent().Name,
+            };
+    }
+}
---
+++ BlueToque.Utility/SystemIdentity.cs
@@ -0,0 +1,40 @@
+﻿namespace BlueToque.Utility
+{
+    public interface IIdentity
+    {
+        string Name { get; }
+        string Issuer { get; }
+    }
+
+    public interface IIdentityProvider
+    {
+        IIdentity GetCurrent();
+    }
+
+    public class DefaultIdentity : IIdentity
+    {
+        public DefaultIdentity()
+        {
+            Name = "Default";
+            Issuer = "Default";
+        }
+
+        public string Name { get; }
+        public string Issuer { get; }
+
+    }
+
+    class DefaultProvider : IIdentityProvider
+    {
+        public IIdentity GetCurrent() => new DefaultIdentity();
+    }
+
+    public class SystemIdentity : Singleton<SystemIdentity> 
+    {
+        public IIdentityProvider Provider { get; private set; } = new DefaultProvider();
+
+        public void Register(IIdentityProvider provider) => Provider = provider;
+
+        public IIdentity GetCurrent() => Provider.GetCurrent();
+    }
+}
